5eedb6a8e189afb71504e1e36618e431009d1b7e
AGK-MM <agkhantzaw.2000@gmail.com>
2026-06-13 23:07:27 +0630

    feat: add exercise 2 and exercise 3

---
+++ 1_basics/exercise2.py
@@ -0,0 +1,5 @@
+product_name = "Eggs"
+price = 1000
+quantity = 12
+total_cost = price * quantity
+print("I bought", quantity, product_name, "for", total_cost, "kyats")
---
+++ 1_basics/exercise3.py
@@ -0,0 +1,8 @@
+student_name = "AGK"
+english_score = 80
+math_score = 90
+total_score = english_score + math_score
+average_score = total_score / 2
+print("Student Name:", student_name)
+print("Total Score:", total_score)
+print("Average Score:", average_score)
