51534510a658a349f9215292185604e213edbcdd
AGK-MM <agkhantzaw.2000@gmail.com>
2026-06-13 23:24:06 +0630

    feat: add more python pratice files

---
+++ 1_basics/exercise4.py
@@ -0,0 +1,8 @@
+employee_name = "AGK"
+monthly_salary = 500000
+bonus = 100000
+total_income = monthly_salary + bonus
+print("Employee Name:", employee_name)
+print("Monthly Salary:", monthly_salary, "kyats")
+print("Bonus:", bonus, "kyats")
+print("Total Income:", total_income, "kyats")
---
+++ 1_basics/exercise5.py
@@ -0,0 +1,4 @@
+length = 50
+width = 30
+area = length * width
+print("The area of the rectangle is:", area, "square feet")
