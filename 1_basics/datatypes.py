e2d68c7a3b053e222bcbd2f2179f28d208a8bff4
AGK-MM <agkhantzaw.2000@gmail.com>
2026-06-15 11:47:54 +0630

    feat: datatypes.py

---
+++ 1_basics/datatypes.py
@@ -0,0 +1,40 @@
+# Integer
+x = 10
+print(x)
+print(type(x))
+
+# Float
+y = 3.23
+print(y)
+print(type(y))
+
+# Complex
+x = 2 + 3
+y = 4j
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+
+# String
+x = "Aung"
+print(x)
+print(type(x))
+
+# Boolean
+x = 2 == 4
+y = 2 != 4
+print(x)
+print(y)
+print(type(x))
+print(type(y))
+
+# Type Conversion
+x = 3.14
+y = int(x)
+print(y)
+print(type(y))
+x = 10
+y = float(x)
+print(y)
+print(type(y))
